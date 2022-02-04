0084b4f83884360149e6c91eaf960664d8274a26
vorabkin <vorabkin@gmail.com>
2022-02-04 15:42:41 +0200

    adds hw36 by Vladimir

--- src/telran/view/InputOutput.java
+++ src/telran/view/InputOutput.java
@@ -1,5 +1,9 @@
 package telran.view;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
@@ -34,4 +38,62 @@ public interface InputOutput {
 		});
 	}
 	//TODO write all default methods from UML schema 
+	// #1
+	default Integer readInt(String prompt) {
+		try {
+			return readObject(prompt, "Input has to include digits only", str -> {
+				return Integer.parseInt(str);
+			});
+		} catch(NumberFormatException ex) {
+			throw new IllegalArgumentException();
+		}
+	}
+	// #2
+	default Integer readInt(String prompt, int min, int max) {
+		String errorMessage = String.format("Digits only, integer in range[%d,%d]", min, max);
+		try {
+			return readObject(prompt, errorMessage, str -> {
+				int value = Integer.parseInt(str);
+				if(value<min || value>max) {
+					throw new IllegalArgumentException();
+				}
+				return value;
+			});
+		} catch(NumberFormatException ex) {
+			throw new IllegalArgumentException();
+		}
+	}
+	// #3
+	default Long readLong(String prompt) {
+		try {
+			return readObject(prompt, "Input has to include digits only", str -> {
+				return Long.parseLong(str);
+			});
+		} catch(NumberFormatException ex) {
+			throw new IllegalArgumentException();
+		}
+	}
+	// #4 readStringOption
+	default String readStringOption(String prompt, Set<String> options) {
+		return readStringPredicate(prompt, "Out of options", str -> {
+			return options.contains(str);
+		});
+	}
+	// # 5 readDate in format YYYY-MM-DD
+	default LocalDate readDate(String prompt) {
+		String formatter = "yyyy-MM-dd";
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern(formatter);
+		return readDate(prompt + " using format " + formatter, dtf);
+	}
+	// # 6 readDate by DateTimeFormatter
+	default LocalDate readDate(String prompt, DateTimeFormatter formatter) {
+		try {
+			return readObject(prompt, "incorrect data format", str -> {
+				return LocalDate.parse(str, formatter);
+			});
+		} catch (DateTimeParseException ex) {
+			throw new IllegalArgumentException();
+		}
+		
+	}
 }
--- src/telran/view/InputOutputTest.java
+++ src/telran/view/InputOutputTest.java
@@ -3,6 +3,10 @@ package telran.view;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,8 +19,10 @@ InputOutput io = new ConsoleInputOutput();
 	void setUp() throws Exception {
 	}
 
-	@Test
+//	@Test
 	void employeeInputAsOneString() {
+		LocalDate d = LocalDate.now();
+		System.out.println(d.toString());
 		Employee empl = io.readObject("Enter employee data as string <id>#<name>#<birthdate ISO>#<salary>#<department>",
 				"Wrong format of employee data", InputOutputTest::toEmployeeFromStr);
 		io.writeObjectLine(empl);
@@ -31,21 +37,45 @@ InputOutput io = new ConsoleInputOutput();
 		return new Employee(id, name, birthDate, salary, department);
 	}
 	
-	@Test 
+//	@Test 
 	void readPredicateTest() {
 		String str = io.readStringPredicate("enter any string containing exactly 3 symbols",
 				"this is no string containing exactly 3 symbols", s -> s.length() == 3);
 		assertEquals(3, str.length());
-	}
+	} 
 	@Test
 	void employeeBySeparateFields() {
 		//TODO
 		//enter ID by readLong method
-		//enter Name by readStringPredicate (only letters with capital first letter)
-		//enter birthdate by readDate 
-		//enter salary by readInt(prompt, min, max)
-		//enter department by readStringOption specifying possible departments
+		long id = io.readLong("Enter employee ID");
+		io.writeObjectLine("employee's id="+id);
 		
+		//enter Name by readStringPredicate (only letters with capital first letter)
+		String name = io.readStringPredicate("enter name, only the letters with capital first letter",
+				"input format is incorrect", s -> s.matches("[A-Z]{1}[a-z]+")); // "[A-Z][a-z]*"
+		io.writeObjectLine("employee's name="+name);
+		
+		//enter birth date by readDate using format "yyy-MM-dd"
+		LocalDate birthDay = io.readDate("Enter employee's birthday ");
+		io.writeObjectLine("Employee's birthday is " + birthDay);
+		
+		//enter birth date by readDate using format "yyyy MM dd"
+		String formatter = "yyyy MM dd";
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern(formatter);
+		birthDay = io.readDate("Enter employee's birthday using format " + formatter, dtf);
+		io.writeObjectLine("Employee's birthday is " + birthDay);
+		
+		//enter salary by readInt(prompt, min, max)
+		int salary = io.readInt("Enter employee's salary", 10000, 20000);
+		io.writeObjectLine("employee's salary="+salary);
+		
+		//enter department by readStringOption specifying possible departments
+		String listOfDepartments[] = {"Engeneering", "QA", "Development"};
+		Set<String> departments = new HashSet<>(Arrays.asList(listOfDepartments));
+		String department = io.readStringOption(
+				"Enter one of the following departments: "+Arrays.toString(listOfDepartments), 
+				departments);
+		io.writeObjectLine("department="+department);
 	}
 
 }
